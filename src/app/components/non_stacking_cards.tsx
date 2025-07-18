5166ba4c3a8006a7ca9fc359fbc8a08587def329
bartek Karpiński <281425@student.pwr.edu.pl>
2025-07-18 16:37:18 +0000

    changes

--- src/app/components/non_stacking_cards.tsx
+++ src/app/components/non_stacking_cards.tsx
@@ -4,7 +4,7 @@ import NonStackingCard from "./non_stacking_card";
 const NonStackingCards = () => {
   return (
     <div id="realizacje" className="px-4 py-6 max-w-md mx-auto">
-      <h2 className="text-2xl font-bold text-white mb-6">Projects</h2>
+      <h2 className="text-2xl font-bold text-white mb-6">Nasze Realizacje</h2>
       <div className="space-y-6">
         {projects.map((project, i) => (
           <NonStackingCard key={i} {...project} />
